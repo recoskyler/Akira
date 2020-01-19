26651c5e796bd321cf552f9ad51f2cd953401364
Adil Atalay Hamamcıoğlu <recoskyler224@gmail.com>
2020-01-20 02:16:20 +0300

    first edits

---
+++ background.js
@@ -0,0 +1,21 @@
+function openTab(filename) {
+    var myID = chrome.i18n.getMessage("@@extension_id");
+    chrome.windows.getCurrent( function(win) { 
+        chrome.tabs.query(
+            {'windowId': win.id}, 
+            function(tabArray) {
+                for(var i in tabArray) {
+                    if(tabArray[i].url == "chrome-extension://" + myID + "/" + filename) { 
+                        console.warn("Already opened");
+                        chrome.tabs.update(tabArray[i].id, {active: true});
+
+                        return; 
+                    }
+                }
+                chrome.tabs.create({url:chrome.extension.getURL(filename)});
+            }
+        );
+    });
+}
+
+openTab("index.html")
