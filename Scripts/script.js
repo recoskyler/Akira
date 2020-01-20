d7844d25bce92a3269780de3e400cad623ef1953
Adil Atalay Hamamcıoğlu <recoskyler224@gmail.com>
2020-01-21 01:35:00 +0300

    Added Popup Added About Page CSS

---
+++ Scripts/script.js
@@ -0,0 +1,64 @@
+var manifestData = chrome.runtime.getManifest();
+
+function aboutScreen() {
+    document.getElementById('main').innerHTML = `
+        <img src="./images/icon512.png" id="logo" alt="AKIRA"/>
+        
+        <p>Akira is a Chrome extension for sorting -> viewing -> bookmarking/closing a bunch of tabs that are open on multiple windows.</p>
+        <p>Made by Recoskyler (Adil Atalay Hamamcıoğlu)</p>
+        <br/>
+        <p>Version: ${manifestData.version}</p>
+    `;
+
+    resetNavClassNames();
+    document.getElementById("aboutScreen").className += " selectedNav";
+}
+
+function openTabsScreen() {
+    document.getElementById('main').innerHTML = `
+        a
+    `;
+
+    resetNavClassNames();
+    document.getElementById("openTabsScreen").className += " selectedNav";
+}
+
+function bookmarksScreen() {
+    document.getElementById('main').innerHTML = `
+        b
+    `;
+
+    resetNavClassNames();
+    document.getElementById("bookmarksScreen").className += " selectedNav";
+}
+
+function recentlyClosedScreen() {
+    document.getElementById('main').innerHTML = `
+        c
+    `;
+
+    resetNavClassNames();
+    document.getElementById("recentlyClosedScreen").className += " selectedNav";
+}
+
+function resetNavClassNames() {
+    document.getElementById("aboutScreen").className = "navButton rightMost";
+    document.getElementById("bookmarksScreen").className = "navButton leftMost";
+    document.getElementById("openTabsScreen").className = "navButton leftMost";
+    document.getElementById("recentlyClosedScreen").className = "navButton leftMost";
+}
+
+window.onload = function() {
+    var els = ["openTabsScreen", "bookmarksScreen", "recentlyClosedScreen", "aboutScreen"];
+    var fus = [this.openTabsScreen, this.bookmarksScreen, this.recentlyClosedScreen, this.aboutScreen];
+
+    for (var i = 0; i < els.length; i++) {
+        var el = document.getElementById(els[i]);
+
+        if (el){
+            el.addEventListener('click', fus[i]);
+        }
+    }
+
+    openTabsScreen();
+}
--- background.js
+++ background.js
@@ -20,6 +20,16 @@ function openTab(filename) {
     });
 }
 
-chrome.browserAction.onClicked.addListener(function(tab) {
-    openTab("popup.html");
+/*
+chrome.runtime.onInstalled.addListener(function() {
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+        chrome.declarativeContent.onPageChanged.addRules([{
+            conditions: [new chrome.declarativeContent.PageStateMatcher({
+                pageUrl: {hostEquals: 'developer.chrome.com'},
+            })
+            ],
+                actions: [new chrome.declarativeContent.ShowPageAction()]
+        }]);
+    });
 });
+*/
